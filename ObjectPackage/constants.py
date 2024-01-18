d6871d3c0bd496092fc7992c2f24fbc01533499d
Christian Jarjat <jarjatc@gmail.com>
2024-01-18 12:31:31 +0000

    Basic pygame set up

---
+++ ObjectPackage/constants.py
@@ -0,0 +1,14 @@
+# width and height of sim window
+WIDTH: int = 800
+HEIGHT: int = 800
+
+# max frame rate of sim
+FRAME_RATE: int = 60
+
+# colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 106, 78)
+BLUE = (100, 149, 237)
+RED = (133, 1, 1)
---
+++ controller.py
@@ -0,0 +1,35 @@
+import pygame as pg
+
+from ObjectPackage.constants import *
+
+# set up the init of pygame
+pg.init()
+
+WIN = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Electric Field Sim")
+
+
+def main():
+    # will cause sim to repeat until user x out of sim
+    run = True
+
+    # sets a max frame rate
+    clock = pg.time.Clock()
+
+    while run:
+        # max fps
+        clock.tick(FRAME_RATE)
+
+
+        WIN.fill(BLACK)
+
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+
+    pg.quit()
+
+
+
+
